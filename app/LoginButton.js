c5974c86daed1ef3f88e5dcdd9ef039579a8da5e
sooheyon <sooheyonjo@gmail.com>
2024-04-18 08:19:02 +0900

    소셜 로그인 기능

---
+++ app/LoginButton.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+
+export default function LoginButton() {
+  return (
+    <button
+      onClick={() => {
+        signIn();
+      }}
+    >
+      로그인
+    </button>
+  );
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   providers: [
-    GithubProviders({
+    GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
     }),
   ],
-  secret: 'soohyeonjo'
+  secret: "soohyeonjo",
 };
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
