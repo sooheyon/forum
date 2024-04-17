ebcbad397fffb9f65e6f0f52d501fe86cc81a0cb
sooheyon <sooheyonjo@gmail.com>
2024-04-17 17:27:26 +0900

    dynamic rendering 적용

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,8 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
+export const dynamic = 'force-dynamic' //force-static
+
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
