27ba165cce46035f563f9806452e68d5111fac9b
sooheyon <sooheyonjo@gmail.com>
2024-04-18 08:41:56 +0900

    글 작성 시 author 추가

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,18 +1,27 @@
 import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
   try {
     if (req.method == "POST") {
       const { title, content } = req.body;
 
+      const session = await getServerSession(req, res, authOptions);
+      if (!session) {
+        return res.status(400).json({ message: "Not exist session" });
+      }
+
       if (!title || !content) {
         return res.status(400).json({ message: "Not exist data" });
       }
 
       const db = (await connectDB).db("forum");
-      const result = await db.collection("post").insertOne({ title, content });
-      return res.status(200).json(result).redirect("/list");
-    } else{
+      const result = await db
+        .collection("post")
+        .insertOne({ title, content, author: session.user.email });
+      return res.status(200).redirect("/list");
+    } else {
       return res.status(400).json({ message: "Not POST" });
     }
   } catch (error) {
